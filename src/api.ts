3b22fe6b8326dc9b00d61629e89602fbfb447b44
최은정 <qhdrn67@naver.com>
2022-02-01 00:05:55 +0900

    Home Screen Banner

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3';
 
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMovieResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`).then(res =>
     res.json(),
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,18 +1,62 @@
 import { useQuery } from 'react-query';
-import { getMovies } from '../api';
+import styled from 'styled-components';
+import { getMovies, IGetMovieResult } from '../api';
+import { makeImagePath } from '../utils';
+
+const Wrapper = styled.div`
+  background: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgPath?: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${props => props.bgPath});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  font-size: 30px;
+  width: 50%;
+`;
 
 function Home() {
-  const { data, isLoading } = useQuery(['movies', 'nowPlaying'], getMovies);
+  const { data, isLoading } = useQuery<IGetMovieResult>(
+    ['movies', 'nowPlaying'],
+    getMovies,
+  );
+  const bgPath = makeImagePath(data?.results[0].backdrop_path || '');
+  const title = data?.results[0].title;
+  const overview = data?.results[0].overview;
 
   return (
-    <div
-      style={{
-        backgroundColor: 'whitesmoke',
-        height: '200vh',
-        paddingTop: 100,
-      }}>
-      {isLoading ? <span>isLoading...</span> : <span>Data is loaded</span>}
-    </div>
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgPath={bgPath}>
+            <Title>{title}</Title>
+            <Overview>{overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
   );
 }
 
