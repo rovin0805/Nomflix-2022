6490ea7f015bc1411884888bfe5c421c3f110ceb
최은정 <qhdrn67@naver.com>
2022-01-31 21:22:47 +0900

    Set Up for Header & Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './routes/Home';
+import Search from './routes/Search';
+import Tv from './routes/Tv.';
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path='/' component={Home} />
+        <Route path='/tv' component={Tv} />
+        <Route path='/search' component={Search} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
